3bd3eea65f7fd6763420932af15d19f0587488ab
Sagrel <Sagrel@users.noreply.github.com>
2021-05-15 19:31:54 +0200

    Ya se puede quitar una entrada

--- src/java/grupo4app/servlet/ServletCargarListaEventosUsuario.java
+++ src/java/grupo4app/servlet/ServletCargarListaEventosUsuario.java
@@ -34,15 +34,7 @@ public class ServletCargarListaEventosUsuario extends HttpServlet {
     private UsuarioEventoFacade usuarioEventoFacade;
     @EJB
     private EventoUsuarioFacade eventoUsuarioFacade;
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String id = request.getParameter("usuario");
